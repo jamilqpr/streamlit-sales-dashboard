7831f76ac015e69397cb294bbc906d4828d4b7ed
jamilqpr <97735105+jamilqpr@users.noreply.github.com>
2023-04-20 05:19:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ total_sales = int(df_selection["Total"].sum())
 average_rating = round(df_selection["Rating"].mean(), 1)
 star_rating = ":star:" * int(round(average_rating, 0))
 average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
-cogs= round(df_selection["cogs"].mean(),1)
-left_column, middle_column, right_column = st.columns(4)
+
+left_column, middle_column, right_column = st.columns(3)
 with left_column:
     st.subheader("Total Sales:")
     st.subheader(f"US $ {total_sales:,}")
@@ -69,9 +69,7 @@ with middle_column:
 with right_column:
     st.subheader("Average Sales Per Transaction:")
     st.subheader(f"US $ {average_sale_by_transaction}")
-with right_column:
-    st.subheader("Average COGS")
-    st.subheader(f"US $ {cogs}")
+
 st.markdown("""---""")
 
 # SALES BY PRODUCT LINE [BAR CHART]
